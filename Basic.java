6f18571a8707f974c8b1e06d6b5b3d360f2ad334
arpit kumar <arpitsingh81027@gmail.com>
2026-02-10 10:26:49 +0530

    reverse

--- Basic.java
+++ Basic.java
@@ -1,8 +1,18 @@
 import java.util.*;
       class Basic{
         public static void main(String[] args) {
+            int rev =0;
             Scanner sc = new Scanner(System.in);
-            System.out.println("hello i am java lang");
+            System.out.print("Enter the number:");
+             int n = sc.nextInt();
+             while(n>0){
+                int d = n%10;
+                rev = d + rev*10;
+                 n = n/10;
+
+             }
+             System.out.println("revrse number is:"+rev);
+
         }
 
 }
